b23479c3468b7866e6c86bc5f70f2e5f749c1f93
mmouchous-ledger <michael.mouchous@ledger.fr>
2023-09-11 11:48:17 +0200

    Wheel event: zoom by using ALT key Panning: use of DragMode.ScrollHandDrag Qt

--- laserstudio/widgets/viewer.py
+++ laserstudio/widgets/viewer.py
@@ -1,6 +1,12 @@
 from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene
-from PyQt6.QtCore import Qt, QPointF
-from PyQt6.QtGui import QBrush, QColorConstants, QWheelEvent, QMouseEvent
+from PyQt6.QtCore import Qt, QPointF, QRectF, QPoint
+from PyQt6.QtGui import (
+    QBrush,
+    QColorConstants,
+    QWheelEvent,
+    QMouseEvent,
+    QGuiApplication,
+)
 from enum import IntEnum, auto
 from typing import Optional, Tuple
 from .stagesight import StageSight
@@ -35,7 +41,7 @@ class Viewer(QGraphicsView):
         # Selection of mode
         self.__mode = Viewer.Mode.NONE
 
-        # Disable scrollbars
+        # Hide scrollbars
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
@@ -43,8 +49,10 @@ class Viewer(QGraphicsView):
         item = self.stage_sight = StageSight()
         self.__scene.addItem(item)
 
-        # Current position and zoom factors where the Viewer is focusing
-        self.__cam_pos_zoom = QPointF(0.0, 0.0), 1.0
+        # Current camera position and zoom factor
+        self.__cam_pos_zoom = QPointF(), 1.0
+
+        self.setInteractive(True)
 
     @property
     def mode(self) -> Mode:
@@ -57,42 +65,51 @@ class Viewer(QGraphicsView):
     def mode(self, new_mode: Mode):
         self.__mode = new_mode
         logging.debug(f"Viewer mode selection: {new_mode}")
+        if new_mode == Viewer.Mode.NONE:
+            self.setDragMode(Viewer.DragMode.NoDrag)
 
     def wheelEvent(self, event: QWheelEvent):
         """
         Handle mouse wheel events to manage zoom.
         """
-        # We want to zoom relative to the current cursor position, not relative
-        # to the center of the widget. This involves some math...
-        # p is the pointed position in the scene, and we want to keep p at the
-        # same screen position after changing the zoom. If c1 and c2 are the
-        # camera before and after, z1 and z2 the zoom levels, then we want:
-        # z1 * (p - c1) = z2 * (p - c2)
-        # which gives:
-        # c2 = c1 * (z1/z2) + p * (1 - z1/z2)
-        # we can use zr = z2/z1, the zoom factor to apply between two events.
+        if Qt.KeyboardModifier.AltModifier in QGuiApplication.queryKeyboardModifiers():
+            # We want to zoom relative to the current cursor position, not relative
+            # to the center of the widget. This involves some math...
+            # p is the pointed position in the scene, and we want to keep p at the
+            # same screen position after changing the zoom. If c1 and c2 are the
+            # camera positions before and after the zoom changes,
+            # z1 and z2 the zoom levels, then we want:
+            # z1 * (p - c1) = z2 * (p - c2)
+            # which gives:
+            # c2 = c1 * (z1/z2) + p * (1 - z1/z2)
+            # we can use zr = z2/z1, the zoom factor to apply.
 
-        p = self.mapToScene(event.position().toPoint())
-        zr = 2 ** ((event.angleDelta().y() * 0.25) / 120)
+            # The pointed position
+            p = self.mapToScene(event.position().toPoint())
 
-        pos, zoom = self.__cam_pos_zoom
-        pos = (pos / zr) + ((1 - (1 / zr)) * p)
-        zoom = zoom * zr
-        self.cam_pos_zoom = pos, zoom
+            # The zoom factor to apply
+            zr = 2 ** (event.angleDelta().y() / (8 * 120))
 
-    def __reset_transform(self):
-        self.resetTransform()
-        pos, zoom = self.cam_pos_zoom
-        self.centerOn(pos)
-        self.scale(zoom, zoom)
+            # Get current position and zoom factor of camera
+            pos, zoom = self.cam_pos_zoom
+
+            pos = (pos / zr) + (p * (1 - (1 / zr)))
+            zoom *= zr
+
+            # Update the position and zoom factors
+            self.cam_pos_zoom = pos, zoom
+            event.accept()
+            return
+
+        super().wheelEvent(event)
 
     @property
     def cam_pos_zoom(self) -> Tuple[QPointF, float]:
-        """Camera position of the viewer: The given point is
-        the position that the viewer is focussing on, the second element
-        of the tuple is the zoom factor, which must be strictly positive.
+        """'Camera' position and zoom of the Viewer: The first element is
+        the position in the stage where the viewer is centered on.
+        The second element is the zoom factor, which must be strictly positive.
 
-        :return: A tuple containing the point where the viewer is focussing
+        :return: A tuple containing the point where the viewer is centered
             on and a float indicating the zoom factor.
         """
         return self.__cam_pos_zoom
@@ -100,38 +117,52 @@ class Viewer(QGraphicsView):
     @cam_pos_zoom.setter
     def cam_pos_zoom(self, new_value: Tuple[QPointF, float]):
         assert new_value[1] > 0
-        self.__cam_pos_zoom = new_value
-        self.__reset_transform()
+        self.__cam_pos_zoom = pos, zoom = new_value
+        logging.debug(self.cam_pos_zoom)
+        self.resetTransform()
+        self.scale(zoom, zoom)
+        self.centerOn(pos)
 
     # User interactions
     def mousePressEvent(self, event: QMouseEvent):
         """
         Called when mouse button is pressed.
         """
-        self.__mouse_prev_pos = event.pos()
+        is_left = event.button() == Qt.MouseButton.LeftButton
+
+        if self.mode == Viewer.Mode.STAGE and is_left:
+            scene_pos = self.mapToScene(event.pos())
+            self.stage_sight.move_to(scene_pos)
+            event.accept()
+            return
+
+        # The event is a press of the right button
+        if event.button() == Qt.MouseButton.RightButton:
+            # Scroll gesture mode
+            self.setDragMode(Viewer.DragMode.ScrollHandDrag)
+            # Transform as left press button event
+            event = QMouseEvent(
+                event.type(),
+                event.position(),
+                Qt.MouseButton.LeftButton,
+                event.buttons(),
+                event.modifiers(),
+                event.pointingDevice(),
+            )
+
+        super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event: QMouseEvent):
         """
         Called when mouse moves.
         In the case where the right button is pressed, moves the position.
         """
-        if self.__mouse_prev_pos is None:
-            self.__mouse_prev_pos = event.pos()
-        delta = event.pos() - self.__mouse_prev_pos
-
-        is_right = Qt.MouseButton.RightButton in event.buttons()
-
-        # Panning management
-        if is_right:
-            cam_pos, cam_zoom = self.cam_pos_zoom
-            cam_pos -= delta.toPointF() / cam_zoom
-            self.cam_pos_zoom = cam_pos, cam_zoom
-
-        self.__mouse_prev_pos = event.pos()
+        super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event: QMouseEvent):
         """
         Called when mouse button is released.
         """
-        if not event.buttons():
-            self.__mouse_prev_pos = None
+        if event.button() == Qt.MouseButton.RightButton:
+            self.setDragMode(Viewer.DragMode.NoDrag)
+        super().mouseReleaseEvent(event)
