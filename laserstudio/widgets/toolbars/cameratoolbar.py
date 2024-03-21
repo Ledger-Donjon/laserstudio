c737e5fc8a7fe91d1cee0f39c22c8f7c930e8aef
Michael Mouchous <michael.mouchous@ledger.fr>
2024-03-21 11:08:01 +0100

    UI implementation of CV2 parameters

--- laserstudio/widgets/toolbars/cameratoolbar.py
+++ laserstudio/widgets/toolbars/cameratoolbar.py
@@ -1,10 +1,21 @@
 from typing import TYPE_CHECKING
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QIcon, QPixmap
-from PyQt6.QtWidgets import QToolBar, QPushButton
-from ...utils.util import resource_path, colored_image
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (
+    QToolBar,
+    QPushButton,
+    QDialog,
+    QWidget,
+    QSizePolicy,
+    QGridLayout,
+    QSlider,
+    QLabel,
+)
+from ...utils.util import colored_image
 from ..stagesight import StageSightViewer, StageSight
 from ..camerawizards import CameraDistortionWizard, ProbesPositionWizard
+from ..return_line_edit import ReturnSpinBox
+from ...instruments.camera_usb import CameraUSBInstrument
 
 if TYPE_CHECKING:
     from ...laserstudio import LaserStudio
@@ -64,3 +75,52 @@ class CameraToolbar(QToolBar):
         w = StageSightViewer(stage_sight)
         w.setHidden(True)
         self.addWidget(w)
+
+        # Refresh interval
+        self.refresh_interval = w = ReturnSpinBox()
+        w.setSuffix("ms")
+        w.setMinimum(20)
+        w.setMaximum(10000)
+        w.setSingleStep(10)
+        w.setValue(self.camera.refresh_interval)
+        w.reset()
+        w.setToolTip("Refresh interval")
+        w.returnPressed.connect(
+            lambda: self.camera.__setattr__(
+                "refresh_interval", self.refresh_interval.value()
+            )
+        )
+        self.addWidget(w)
+
+        # Image adjustment dialog (for USB camera)
+        if isinstance(self.camera, CameraUSBInstrument):
+            self.image_dialog = QDialog()
+            self.image_dialog.setWindowTitle("Image Adjustment")
+            sliders = QWidget(self.image_dialog)
+            sliders.setSizePolicy(
+                QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding
+            )
+            grid = QGridLayout()
+            sliders.setLayout(grid)
+            for i, (att, minimum, maximum) in enumerate(
+                [
+                    ("brightness", 0, 255),
+                    ("contrast", 0, 31),
+                    ("saturation", 0, 31),
+                    ("hue", -180, 180),
+                    ("gamma", 0, 127),
+                    ("sharpness", 0, 15),
+                ]
+            ):
+                grid.addWidget(QLabel(f"{att.capitalize()}:"), i, 0)
+                w = QSlider(Qt.Orientation.Horizontal)
+                w.setMinimum(minimum)
+                w.setMaximum(maximum)
+                w.setValue(int(getattr(self.camera, att)))
+                w.valueChanged.connect(
+                    lambda x, _att=att: setattr(self.camera, _att, x)
+                )
+                grid.addWidget(w, i, 1)
+            w = QPushButton(self.image_dialog.windowTitle())
+            w.clicked.connect(lambda: self.image_dialog.exec())
+            self.addWidget(w)
