e3028e31c64ee57672eb62e35ce6f7537a079c32
Michael Mouchous <michael.mouchous@ledger.fr>
2024-03-21 11:07:31 +0100

    Add access to CV2 parameters for cameras

--- laserstudio/instruments/camera_usb.py
+++ laserstudio/instruments/camera_usb.py
@@ -48,3 +48,95 @@ class CameraUSBInstrument(CameraInstrument):
             size = self.width, self.height
             data = self.cv2.resize(data, size, interpolation=self.cv2.INTER_AREA)
         return self.width, self.height, "RGB", bytes(data)
+
+    @property
+    def brightness(self) -> float:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_BRIGHTNESS)
+        return float(exp)
+
+    @brightness.setter
+    def brightness(self, value: float):
+        self.__video_capture.set(self.cv2.CAP_PROP_BRIGHTNESS, value)
+
+    @property
+    def contrast(self) -> float:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_CONTRAST)
+        return float(exp)
+
+    @contrast.setter
+    def contrast(self, value: float):
+        self.__video_capture.set(self.cv2.CAP_PROP_CONTRAST, value)
+
+    @property
+    def exposure(self) -> float:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_EXPOSURE)
+        return float(exp)
+
+    @exposure.setter
+    def exposure(self, value: float):
+        self.__video_capture.set(self.cv2.CAP_PROP_EXPOSURE, value)
+
+    @property
+    def gain(self) -> float:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_GAIN)
+        return float(exp)
+
+    @gain.setter
+    def gain(self, value: float):
+        self.__video_capture.set(self.cv2.CAP_PROP_GAIN, value)
+
+    @property
+    def hue(self) -> float:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_HUE)
+        return float(exp)
+
+    @hue.setter
+    def hue(self, value: float):
+        self.__video_capture.set(self.cv2.CAP_PROP_HUE, value)
+
+    @property
+    def saturation(self) -> float:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_SATURATION)
+        return float(exp)
+
+    @saturation.setter
+    def saturation(self, value: float):
+        self.__video_capture.set(self.cv2.CAP_PROP_SATURATION, value)
+
+    @property
+    def fps(self) -> int:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_FPS)
+        return int(exp)
+
+    @fps.setter
+    def fps(self, value: int):
+        self.__video_capture.set(self.cv2.CAP_PROP_FPS, value)
+
+    @property
+    def sharpness(self) -> int:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_SHARPNESS)
+        return int(exp)
+
+    @sharpness.setter
+    def sharpness(self, value: int):
+        self.__video_capture.set(self.cv2.CAP_PROP_SHARPNESS, value)
+
+    @property
+    def gamma(self) -> int:
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_GAMMA)
+        return int(exp)
+
+    @gamma.setter
+    def gamma(self, value: int):
+        self.__video_capture.set(self.cv2.CAP_PROP_GAMMA, value)
+
+    def set_gain(self, low: int, high: int):
+        # TODO: Untested yet
+        gain = (low + high) // 2
+        self.__video_capture.set(self.cv2.CAP_PROP_EXPOSURE, gain)
+
+    def gain_autoset(self) -> tuple[int, int]:
+        # TODO: Untested yet
+        exp = self.__video_capture.get(self.cv2.CAP_PROP_EXPOSURE)
+        exp = int(exp)
+        return exp, exp
