a587c8293566542ba85bf3febb7e8842cc0ae38a
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2024-08-16 14:07:07 +0200

    Added autofocus toolbar
    Added a toolbar to:
    - register focus positions
    - apply focus based on registered points
    - run autofocus search
    
    Autofocus uses laplacian standard deviation on the camera's image to
    find best focus position.

--- laserstudio/laserstudio.py
+++ laserstudio/laserstudio.py
@@ -25,6 +25,7 @@ from .widgets.toolbars import (
     LaserDriverToolbar,
     CameraNITToolBar,
     HayashiLightToolbar,
+    FocusToolbar,
 )
 import yaml
 from .restserver.server import RestProxy
@@ -91,6 +92,11 @@ class LaserStudio(QMainWindow):
         if self.instruments.stage is not None:
             toolbar = StageToolbar(self)
             self.addToolBar(Qt.ToolBarArea.BottomToolBarArea, toolbar)
+
+        # Toolbar: Focusing
+        if (self.instruments.stage is not None) and (self.instruments.camera is not None):
+            toolbar = FocusToolbar(self.instruments.stage, self.instruments.camera)
+            self.addToolBar(toolbar)
 
         # Toolbar: Scanning zone definition and usage
         toolbar = ScanToolbar(self)
