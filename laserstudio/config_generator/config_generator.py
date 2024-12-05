def8fc6a9ab14fc126b94663194ca9684fd22834
mmouchous-ledger <michael.mouchous@ledger.fr>
2024-12-05 10:58:38 +0100

    Continue implementation

--- laserstudio/config_generator/config_generator.py
+++ laserstudio/config_generator/config_generator.py
@@ -11,6 +11,44 @@ import yaml
 logger = logging.getLogger("Config Generator")
 
 
+def prompt_key(
+    key: str, type: str, hint: Optional[str], required=False
+) -> Optional[Union[Union[Union[str, int], float], bool]]:
+    if type not in ["string", "integer", "number", "boolean"]:
+        raise ValueError(f"Unsupported schema type: {type}")
+
+    x = "?"
+    while x.startswith("?"):
+        x = input(
+            f"For property {bold(key)}, enter a{'n' if type=='integer' else ''} {bold(type)} value{' (true/false)' if type=='boolean' else ''}: "
+        )
+        if x.startswith("?"):
+            if hint is None:
+                print(f"No description for property {bold(key)} is available.")
+                x = ""
+                break
+            print(f"Description: {hint}")
+
+    if x == "" and not required:
+        return None
+
+    try:
+        if type == "integer":
+            x = int(x)
+        elif type == "number":
+            x = float(x)
+        elif type == "boolean":
+            if x.lower() not in ["true", "false"]:
+                raise ValueError(f"{x} is an invalid boolean value")
+            x = x.lower() == "true"
+        else:
+            return x
+        return x
+
+    except Exception as e:
+        logger.error(f"Invalid value for property {bold(key)}: {e}")
+        return prompt_key(key, type, hint)
+
 
 def bold(s: str) -> str:
     return Style.BRIGHT + s + Style.RESET_ALL
@@ -54,30 +92,30 @@ def generate_array_interactive(schema: dict[str, Any], key: str):
     if item_schema.get("type") == "object":
         name = item_schema.get("title", key)
         result = []
-        # The user will be prompted to instanciate or not the object
         while True:
-            subresult = generate_json_interactive(item_schema, name, ask_user=True)
+            subresult = generate_json_interactive(item_schema, name)
             if subresult is None:
                 break
             assert type(subresult) is dict
             result.append(subresult)
         return result
     else:
-        result = prompt(
-            f"Give a list of {item_schema.get('type')}, with comma-separated values: "
+        result = input(
+            f"For property {bold(key)}, give a list of {bold(item_schema.get('type'))}s, with comma-separated values: "
         )
         # TODO Validate the input according to type
+        if result == "":
+            return None
         return result.split(",")
 
 
 # Function to generate JSON data interactively based on the schema
-def generate_json_interactive(schema: dict[str, Any], key: str = "???", ask_user=False):
-    # Identify which type of schema we are dealing with
+def generate_json_interactive(schema: dict[str, Any], key):
+    # Identify which type of element we are dealing with
     element_type = schema.get("type")
 
     # If the schema does not have a 'type' key, assume it is an object
-    if key in ["camera", "stage"]:
-        ask_user = True
+    ask_user = key in ["camera", "stage", "lasers", "probes"]
 
     if element_type is None:
         logger.debug(json.dumps(schema, indent=2))
@@ -86,33 +124,82 @@ def generate_json_interactive(schema: dict[str, Any], key: str = "???", ask_user
         )
         element_type = "object"
 
+    name = (
+        schema.get("items", {}).get("title", key)
+        if element_type == "array"
+        else schema.get("title", key)
+    )
+
     # Give the possibiltiy not to instanciate an object
-    if ask_user and element_type == "object":
-        name = schema.get("title", key)
-        if prompt(f"Do you want to instanciate a '{name}'? (y/n) ") == "n":
+    if ask_user:
+        if (
+            x := input(f"Do you want to instanciate a {bold(name)}? (y/N) ")
+        ) == "" or x.strip().lower().startswith("n"):
             return None
 
     result = {}
 
     # Generate multiple schemas from the "allOf" list
+    # Should not exist anymore, as the schema is "flattened" in ref_resolve.py
     if "allOf" in schema:
         # The "allOf" list contains multiple schemas that must all be satisfied
         logger.info(f"Satisfy {len(schema['allOf'])} schemas in 'allOf'")
         for subschema in schema["allOf"]:
-            subresult = generate_json_interactive(subschema)
+            subresult = generate_json_interactive(subschema, key)
             assert type(subresult) is dict
             result.update(subresult)
 
+    # Generate schema from the "anyOf" list
+    if "anyOf" in schema:
+        if len(schema["anyOf"]) == 1:
+            # The "anyOf" list contains only one schema
+            subresult = generate_json_interactive(schema["anyOf"][0], key)
+            assert type(subresult) is dict
+            result.update(subresult)
+        else:
+            # The "anyOf" list contains multiple schemas and the user must choose at least one
+            has_instanciated_one_schema = False
+            while not has_instanciated_one_schema:
+                print(
+                    f"There are {len(schema['anyOf'])} possible schemas for {bold(name)}, you have to instanciate {bold('at least one')}:"
+                )
+                for i, option in enumerate(schema["anyOf"]):
+                    print(
+                        bold(f"Schema {i + 1}: ")
+                        + describe_schema_properties(option, name)
+                    )
+                for i, option in enumerate(schema["anyOf"]):
+                    if (
+                        x := input(f"Do you want to instanciate Schema {i}? (y/N) ")
+                    ) == "" or x.strip().lower().startswith("n"):
+                        continue
+                    subresult = generate_json_interactive(option, key)
+                    if type(subresult) is dict:
+                        result.update(subresult)
+                        has_instanciated_one_schema = True
+                if not has_instanciated_one_schema:
+                    print(
+                        f"You must instanciate {bold('at least one')} of proposed schema..."
+                    )
+
     # Generate one schema from the "oneOf" list
     if "oneOf" in schema:
-        # The "oneOf" list contains multiple schemas and the user must choose one
-        print("Choose one of the following options:")
-        for i, option in enumerate(schema["oneOf"]):
-            print(f"Option {i + 1}: {json.dumps(option, indent=2)}")
-        choice = int(prompt("Enter the option number: ")) - 1
-        subresult = generate_json_interactive(schema["oneOf"][choice])
-        assert type(subresult) is dict
-        result.update(subresult)
+        if len(schema["oneOf"]) > 1:
+            # The "oneOf" list contains multiple schemas and the user must choose one
+            print(
+                f"There are {len(schema['oneOf'])} possible schemas {('for ' + bold(name)) if name else key if key else 'to instanciate'}, choose one of the following:"
+            )
+            for i, option in enumerate(schema["oneOf"]):
+                print(
+                    bold(f"Schema {i + 1}: ") + describe_schema_properties(option, name)
+                )
+            choice = int(input("Enter the schema number: ")) - 1
+        else:
+            choice = 0
+
+        subresult = generate_json_interactive(schema["oneOf"][choice], "")
+        if type(subresult) is dict:
+            result.update(subresult)
 
     # Array of objects
     if element_type == "array":
@@ -124,18 +211,18 @@ def generate_json_interactive(schema: dict[str, Any], key: str = "???", ask_user
 
     # Handle enums and constants
     if "enum" in schema:
-        print("Choose one of following values:")
+        print(f"For property {bold(key)}, choose one of following values:")
         for i, value in enumerate(schema["enum"]):
             print(f"{i + 1}: {value}")
-        choice = int(prompt("Enter the option number: ")) - 1
+        choice = int(input("Enter the option number: ")) - 1
         return schema["enum"][choice]
 
-    elif "const" in schema:
-        logger.info(f"{key}'s value must be {schema['const']}.")
+    if "const" in schema:
+        logger.info(
+            f"For property {bold(key)}, value is constant and will be {bold(schema['const'])}."
+        )
         return schema["const"]
 
-    elif element_type == "string":
-        return prompt("Enter a string value: ")
     # Handle object by generating its properties
     if element_type == "object":
         for key, subschema in schema.get("properties", {}).items():
@@ -151,15 +238,10 @@ def generate_json_interactive(schema: dict[str, Any], key: str = "???", ask_user
             print(f"{bold(name)} instanciated.\n")
         return result
 
-    elif element_type == "integer":
-        return int(prompt("Enter an integer value: "))
+    # Handle simple types (string, integer, number, boolean)
+    return prompt_key(key, element_type, schema.get("description"))
 
-    elif element_type == "number":
-        return float(prompt("Enter a number value: "))
 
-    elif element_type == "boolean":
-        value = prompt("Enter a boolean value (true/false): ").lower()
-        return value == "true"
 def main(
     schema_uri="config.schema.json",
     base_url="https://raw.githubusercontent.com/Ledger-Donjon/laserstudio/main/config_schema/",
@@ -184,7 +266,7 @@ def main(
     logger.info("\n" * 2)
 
     # Generate JSON data interactively based on the schema
-    generated_json = generate_json_interactive(schema)
+    generated_json = generate_json_interactive(schema, "")
 
     # Print the generated JSON data
     print("\nGenerated config.yaml:")
@@ -198,8 +280,6 @@ def main(
         logger.error(f"Generated JSON is not valid: {e.message}")
         return -1
 
-    # Print the generated JSON data
-    logger.debug(json.dumps(generated_json, indent=2))
     return 0
 
 
