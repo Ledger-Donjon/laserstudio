632fdf9f5c4893f0b493d97019901232dcee3462
mmouchous-ledger <michael.mouchous@ledger.fr>
2024-12-11 18:14:28 +0100

    Search for a configuration file from current directory up to root folder. If not found, triggers the interactive generation of configuration file

--- laserstudio/__main__.py
+++ laserstudio/__main__.py
@@ -10,6 +10,7 @@ import logging
 import argparse
 from .utils.util import resource_path
 from .utils.colors import LedgerColors
+from .config_generator import ConfigGenerator
 
 
 def main():
@@ -19,7 +20,7 @@ def main():
     parser.add_argument(
         "--log", choices=list(logging._nameToLevel.keys()), required=False
     )
-    parser.add_argument("--conf_file", type=open, required=False)
+    parser.add_argument("--conf_file", type=argparse.FileType("r"), required=False)
     args = parser.parse_args()
 
     if args.log is not None:
@@ -30,12 +31,6 @@ def main():
         except ValueError as e:
             print("Warning, error during setting log level:", e)
             pass
-
-    # Get existing configuration file
-    if os.path.exists("config.yaml"):
-        yaml_config = yaml.load(open("config.yaml", "r"), yaml.FullLoader)
-    else:
-        yaml_config = None
 
     app.setApplicationName("Laser Studio")
     app.setApplicationDisplayName("Laser Studio")
@@ -64,14 +59,43 @@ def main():
     palette.setColor(QPalette.ColorRole.Highlight, LedgerColors.SafetyOrange.value)
     palette.setColor(QPalette.ColorRole.HighlightedText, QColor(255, 255, 255))
     app.setPalette(palette)
-    app.setStyleSheet("QToolBar { "
+    app.setStyleSheet(
+        "QToolBar { "
         "border: 1px solid #252525;"
         "border-radius: 4px;"
         "margin: 3px;"
         "padding: 6px;"
-        "background-color: #252525 }")
+        "background-color: #252525 }"
+    )
 
     QLocale.setDefault(QLocale.c())
+
+    # Loading configuration file
+    stream = None
+    if args.conf_file is not None:
+        stream = args.conf_file
+    else:
+        # Search existing configuration file
+        current_dir = os.path.realpath((os.curdir))
+        while not os.path.exists(os.path.join(current_dir, "config.yaml")):
+            parent_dir = os.path.dirname(current_dir)
+            if current_dir == parent_dir:
+                break
+            current_dir = parent_dir
+
+        if os.path.exists(path := os.path.join(current_dir, "config.yaml")):
+            stream = open(path, "r")
+
+    if stream is not None:
+        yaml_config = yaml.load(stream, yaml.FullLoader)
+    else:
+        config_generator = ConfigGenerator(
+            base_url="/Volumes/Work/Gits/Ledger-Donjon/laserstudio/config_schema"
+        )
+        config_generator.load_schema()
+        config_generator.print_intro()
+        yaml_config = config_generator.generate_json_interactive()
+        assert yaml_config is None or type(yaml_config) is dict
 
     win = LaserStudio(yaml_config)
     win.setWindowTitle(app.applicationDisplayName())
--- laserstudio/config_generator/__init__.py
+++ laserstudio/config_generator/__init__.py
@@ -1,1 +1,4 @@
 from .config_generator import main
+from .config_generator import ConfigGenerator
+
+__all__ = ["main", "ConfigGenerator"]
