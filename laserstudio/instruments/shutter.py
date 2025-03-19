01cc8b126d9c1b5a6dfb30c367cad2803a2a74c1
mmouchous-ledger <michael.mouchous@ledger.fr>
2025-03-19 18:54:50 +0100

    Create daughter class TicShutterInstrument

--- laserstudio/instruments/shutter.py
+++ laserstudio/instruments/shutter.py
@@ -5,10 +5,7 @@ from .instrument import Instrument
 class ShutterInstrument(Instrument):
     def __init__(self, config: dict):
         super().__init__(config=config)
-        self.tic = Tic()
-        self.tic.exit_safe_start()
-        self.tic.go_home(TicDirection.FORWARD)
-        self.__open = True  # Open after homing
+        self.__open = True  # Open by default
 
     @property
     def open(self) -> bool:
@@ -18,6 +15,23 @@ class ShutterInstrument(Instrument):
     def open(self, value: bool):
         if type(value) is not bool:
             raise ValueError("Expected bool")
+        self.__open = value
+
+
+class TicShutterInstrument(ShutterInstrument):
+    def __init__(self, config: dict):
+        super().__init__(config=config)
+        self.tic = Tic()
+        self.tic.exit_safe_start()
+        self.tic.go_home(TicDirection.FORWARD)
+        self.__open = True  # Open after homing
+
+    @property
+    def open(self) -> bool:
+        return super().open
+
+    @open.setter
+    def open(self, value: bool):
+        super().open = value
         if value != self.__open:
             self.tic.position = Vector({False: -106, True: 0}[value])
-            self.__open = value
