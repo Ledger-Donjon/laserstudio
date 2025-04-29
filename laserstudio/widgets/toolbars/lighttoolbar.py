f3ad5689f27fdcaf5627c2eefba6fbd09d844d33
mmouchous-ledger <michael.mouchous@ledger.fr>
2025-04-29 11:55:14 +0200

    update icon

--- laserstudio/widgets/toolbars/lighttoolbar.py
+++ laserstudio/widgets/toolbars/lighttoolbar.py
@@ -55,7 +55,7 @@ class LightToolBar(QToolBar):
 
         if type(light) is LMSControllerInstrument:
             w = ColoredPushButton(
-                ":/icons/shutter-open2.svg", ":/icons/shutter-closed.svg"
+                ":/icons/shutter-open.svg", ":/icons/shutter-closed.svg"
             )
             w.setToolTip("Open/Close shutter")
             w.setCheckable(True)
