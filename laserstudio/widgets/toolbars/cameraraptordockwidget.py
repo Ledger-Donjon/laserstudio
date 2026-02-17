af3d67bc195ad7cf593052243581217851dbd75f
Michael <michael.mouchous@ledger.fr>
2026-02-17 13:32:45 +0100

    chore: clean up imports in cameraraptordockwidget.py

--- laserstudio/widgets/toolbars/cameraraptordockwidget.py
+++ laserstudio/widgets/toolbars/cameraraptordockwidget.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-import logging
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 from ...instruments.camera_raptor import (
     CameraRaptorInstrument,
     RaptorCameraControlReg0,
