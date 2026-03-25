04159a6441bbebdf1bc1acd7323f0593e1c18865
mmouchous-ledger <michael.mouchous@ledger.fr>
2026-03-25 15:33:38 +0100

    refactor(typing): Resolve Vector typing confusion

--- laserstudio/instruments/stage_dummy.py
+++ laserstudio/instruments/stage_dummy.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from pystages import Stage
+from pystages import Stage, Vector
 from typing import cast, TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
-    from .stage import StageInstrument, Vector
+    from .stage import StageInstrument
 
 
 class StageDummy(Stage):
@@ -18,15 +18,13 @@ class StageDummy(Stage):
         super().__init__(num_axis=cast(int, config.get("num_axis", 2)))
         from .stage import Vector as LSVector
 
-        self._position = LSVector(dim=self.num_axis)
+        self._position: Vector = LSVector(dim=self.num_axis)
         self.stage_instrument = stage_instrument
 
     @property
     def position(self) -> Vector:
         # Do not return the object itself, but a copy
-        from .stage import Vector as LSVector
-
-        return LSVector(*self._position.data)
+        return Vector(*self._position.data)
 
     @position.setter
     def position(self, value: Vector):
