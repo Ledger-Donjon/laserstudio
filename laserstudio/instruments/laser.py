286bf807e23111ac3f9dc7903ad211c49200be06
Michael <michael.mouchous@ledger.fr>
2026-05-29 09:03:00 +0200

    refactor(typing): enhance type annotations and validation in LaserInstrument class for improved clarity and error handling. Solves issue Shutter of laser is not appearing Fixes #61

--- laserstudio/instruments/laser.py
+++ laserstudio/instruments/laser.py
@@ -1,14 +1,14 @@
-from .probe import ProbeInstrument
 from random import uniform
 from PyQt6.QtCore import QVariant
-from .shutter import ShutterInstrument
-from typing import Optional, Any
-from .lmscontroller import LMSControllerInstrument
 import logging
+from .probe import ProbeInstrument
+from .shutter import ShutterInstrument
+from .lmscontroller import LMSControllerInstrument
+from ..utils.yaml_types import Config
 
 
 class LaserInstrument(ProbeInstrument):
-    def __init__(self, config: dict[str, Any]):
+    def __init__(self, config: Config):
         super().__init__(config=config)
         # Sweep parameters, in order to change the current_percentage
         # regularly, within a random value from sweep_min to sweep_max,
@@ -19,9 +19,9 @@ class LaserInstrument(ProbeInstrument):
         self._sweep_iteration = 0
 
         # Shutter
-        self.shutter: Optional[ShutterInstrument] = None
+        self.shutter: ShutterInstrument | None = None
         shutter = config.get("shutter")
-        if type(shutter) is dict[str, Any] and shutter.get("enable", True):
+        if isinstance(shutter, dict) and shutter.get("enable", True):
             try:
                 device_type = shutter.get("type")
                 if device_type == "LMSController":
@@ -56,14 +56,14 @@ class LaserInstrument(ProbeInstrument):
     def offset_current(self, value: float):
         self.parameter_changed.emit("offset_current", QVariant(value))
 
-    def go_next(self) -> dict[str, Any]:
+    def go_next(self) -> Config:
         self._sweep_iteration += 1
         if self.sweep_freq and (self._sweep_iteration % self.sweep_freq) == 0:
             self.current_percentage = uniform(self.sweep_min, self.sweep_max)
         return {"current_percentage": self.current_percentage}
 
     @property
-    def settings(self) -> dict[str, Any]:
+    def settings(self) -> Config:
         """
         Return a dict of settings for the PDM.
         """
@@ -81,28 +81,59 @@ class LaserInstrument(ProbeInstrument):
         return super_settings
 
     @settings.setter
-    def settings(self, data: dict[str, Any]):
+    def settings(self, data: Config):
         """
         Set the settings of the PDM.
         """
         ProbeInstrument.settings.__set__(self, data)
         if "on_off" in data:
-            self.on_off = data["on_off"]
+            self.on_off = bool(data["on_off"])
             self.parameter_changed.emit("on_off", data["on_off"])
         if "current_percentage" in data:
-            self.current_percentage = data["current_percentage"]
-            self.parameter_changed.emit(
-                "current_percentage", data["current_percentage"]
-            )
+            current_percentage = data["current_percentage"]
+            if isinstance(current_percentage, float):
+                self.current_percentage = current_percentage
+                self.parameter_changed.emit("current_percentage", current_percentage)
+            else:
+                logging.getLogger("laserstudio").warning(
+                    f"Current percentage is not a float: {current_percentage}. Skipping."
+                )
         if "offset_current" in data:
-            self.offset_current = data["offset_current"]
-            self.parameter_changed.emit("offset_current", data["offset_current"])
+            offset_current = data["offset_current"]
+            if isinstance(offset_current, float):
+                self.offset_current = offset_current
+                self.parameter_changed.emit("offset_current", offset_current)
+            else:
+                logging.getLogger("laserstudio").warning(
+                    f"Offset current is not a float: {offset_current}. Skipping."
+                )
         if "sweep_max" in data:
-            self.sweep_max = data["sweep_max"]
+            sweep_max = data["sweep_max"]
+            if isinstance(sweep_max, float):
+                self.sweep_max = sweep_max
+                self.parameter_changed.emit("sweep_max", sweep_max)
+            else:
+                logging.getLogger("laserstudio").warning(
+                    f"Sweep max is not a float: {sweep_max}. Skipping."
+                )
             self.parameter_changed.emit("sweep_max", data["sweep_max"])
         if "sweep_min" in data:
-            self.sweep_min = data["sweep_min"]
+            sweep_min = data["sweep_min"]
+            if isinstance(sweep_min, float):
+                self.sweep_min = sweep_min
+                self.parameter_changed.emit("sweep_min", sweep_min)
+            else:
+                logging.getLogger("laserstudio").warning(
+                    f"Sweep min is not a float: {sweep_min}. Skipping."
+                )
             self.parameter_changed.emit("sweep_min", data["sweep_min"])
         if "sweep_freq" in data:
-            self.sweep_freq = data["sweep_freq"]
+            sweep_freq = data["sweep_freq"]
+            if isinstance(sweep_freq, int):
+                self.sweep_freq = int(sweep_freq)
+                self.parameter_changed.emit("sweep_freq", sweep_freq)
+            else:
+                logging.getLogger("laserstudio").warning(
+                    f"Sweep freq is not an integer: {sweep_freq}. Skipping."
+                )
             self.parameter_changed.emit("sweep_freq", data["sweep_freq"])
